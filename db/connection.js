6e7961c6205868c8250826a1d7e3af71fb41a4f0
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-07-02 14:07:07 +0100

    completed setup for supabase

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,11 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
